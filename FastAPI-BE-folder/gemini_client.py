cb8078d51d61521e9273977a5bb54a7783b6828e
tannisthamaiti <37048897+tannisthamaiti@users.noreply.github.com>
2025-08-18 19:16:12 -0400

    Add files via upload

--- FastAPI-BE-folder/gemini_client.py
+++ FastAPI-BE-folder/gemini_client.py
@@ -2,7 +2,7 @@ import asyncio
 import httpx
 
 # --- Configuration ---
-API_KEY = "AIzaSyCkNLwnE7N0R4pGd5HluJ9eAZB17LMGl5s"
+API_KEY = ""
 MODEL_NAME = "gemini-2.5-flash-preview-05-20"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL_NAME}:generateContent?key={API_KEY}"
 
